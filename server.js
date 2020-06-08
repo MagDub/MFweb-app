bde2e0c45d1b7b1506939610361477f6490ff149
Magda Dubois <magda.dubois.18@ucl.ac.uk>
2020-06-08 19:08:39 +0200

    server for scalingo

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path    = require('path');
+const app     = express();
+const port = process.env.PORT
+// app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+-app.get('/', function (req, res) {
+	+app.get('/*', function (req, res) {
+   	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ 	});
+
+});
+
+// app.listen(9000);
+app.listen(port, () => console.log("Listening on Port", port));
